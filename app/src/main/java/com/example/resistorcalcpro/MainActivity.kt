0377a68453ebe3c2cd45646e96b78c1f31080373
djpaesgana <djpaesgana@hotmail.com>
2026-01-01 21:18:43 -0600

    Release version 0.7: Accessibility fixes and version update

--- app/src/main/java/com/example/resistorcalcpro/MainActivity.kt
+++ app/src/main/java/com/example/resistorcalcpro/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.resistorcalcpro
 
 import android.os.Bundle
 import android.webkit.WebView
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,18 +13,23 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Habilitar modo pantalla completa inmersiva
         enableEdgeToEdge()
-
+        
         val myWebView = WebView(this)
         myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.domStorageEnabled = true
+        
+        // Hacer que el contenido ignore las barras del sistema para usar TODA la pantalla
+        myWebView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+
         setContentView(myWebView)
-
-        ViewCompat.setOnApplyWindowInsetsListener(myWebView) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         myWebView.loadUrl("file:///android_asset/index.html")
     }
 }
